c879fec13d49785bb2743328be04464a79aab216
Hasushi <hasuprog1225@gmail.com>
2025-03-04 00:51:40 +0900

    coreDB

---
+++ config/dsn.go
@@ -0,0 +1,10 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+func DSN() string{
+	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
+}
